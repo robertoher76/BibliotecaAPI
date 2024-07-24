7793dc04b27ab24f1ffccbe67930fe183bcbcacd
robertoher76 <robertoher76@gmail.com>
2024-07-24 14:43:59 -0600

    AddServiceConnector() to Program.cs in Biblioteca.API

--- Biblioteca.API/Program.cs
+++ Biblioteca.API/Program.cs
@@ -1,4 +1,5 @@
 using Biblioteca.Common;
+using Biblioteca.BL.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddServiceConnector();
 
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 
